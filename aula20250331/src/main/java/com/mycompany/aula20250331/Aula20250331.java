d47d233667073acfcfae37a61d395a36cf366729
spacefy1 <161043737+spacefy1@users.noreply.github.com>
2025-03-31 10:42:28 -0300

    Add files via upload

---
+++ aula20250331/src/main/java/com/mycompany/aula20250331/Aula20250331.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula20250331;
+
+/**
+ *
+ * @author alunolab02
+ */
+public class Aula20250331 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
